24fa2b2c216c56942d4f8aee61a13887ab0ac8ef
Joven Pancho <jovenlin28@gmail.com>
2021-08-17 16:37:39 +0800

    feat: implement chip creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'take-home-test';
+export class AppComponent implements OnInit {
+
+  public activeSort: string;
+  public chips: string[] = [];
+  public nameModel: string;
+
+  ngOnInit(): void {
+
+  }
+
+  public addName(): void {
+    // don't add a name in the chips if it already exists
+    if (!this.chips.includes(this.nameModel)) {
+
+      // Reassignment instead of using push method to 
+      // respect OnPush change detection strategy of child component
+      this.chips = [...this.chips, this.nameModel];
+      this.nameModel = '';
+    }
+  }
+
+  public onSortChange(event): void {
+    console.log(event.target.value);
+  }
 }
